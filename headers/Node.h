f7819ef1079cbe15cb81391b8ef20eb4945c3b16
Ananya Sood <asood010@xe-01.cs.ucr.edu>
2024-10-30 12:57:11 -0700

    fixed error with Node.h class

---
+++ headers/Node.h
@@ -0,0 +1,43 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include "../headers/Problem.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+enum ShiftDirection { NONE, LEFT, UP, RIGHT, DOWN };
+
+class Node {
+private:
+    Problem state;
+    Node* parent;
+    int g_cost;
+    int h_cost;x
+    int depth;
+    //to help keep track of the move direction from current state to parent state
+    ShiftDirection move;
+
+    //helper private function for calculating hueristic cost
+    int calculateHeuristic(const Problem& state) const;
+
+public:
+    //initializing everyrhting to nothing (constructor, intial state)
+    Node (const Problem& state, Node* parent = nullptr, int g_cost = 0, int h_cost = 0, ShiftDirection move = ShiftDirection::NONE);
+
+    int getTotalCost() const;
+    const Problem& getState() const;
+    Node* getParent() const;
+    int getGCost() const;
+    int getHCost() const;
+    int getDepth() const;
+    ShiftDirection getMove() const;
+
+    void printState() const;
+    //create and return all child nodes from current node states
+    //for A* and Uniform Cost Search
+    vector<Node*> generateChildren() const;
+};
+
+#endif // NODE_H
